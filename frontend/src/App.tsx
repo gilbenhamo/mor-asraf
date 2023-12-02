6f99e0c22715e898ed59edf1d5267b9ffbaa2aa7
gilbenhamo <gilbh859@gmail.com>
2023-12-02 17:28:33 +0200

    Added all dependecies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,10 @@
+import { motion } from "framer-motion";
 import "./App.css";
 
 function App() {
   return (
     <>
-      <p className=" text-black">the Vite and React logos to learn more</p>
+      <motion.p whileHover={{scale:1.3}} className=" text-black">the Vite and React logos to learn more</motion.p>
     </>
   );
 }
