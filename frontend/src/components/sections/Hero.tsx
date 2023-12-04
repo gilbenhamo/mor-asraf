fce4f05862223fd9b198f01b6600b043fdac83ab
gilbenhamo <gilbh859@gmail.com>
2023-12-04 23:46:19 +0200

    fixed bug in hero section - propotion

--- frontend/src/components/sections/Hero.tsx
+++ frontend/src/components/sections/Hero.tsx
@@ -2,17 +2,37 @@ import { motion, useScroll, useTransform } from "framer-motion";
 import { OvalNextSection } from "../UI/OvalNextSection";
 
 export const Hero = () => {
-  const screen_height = window.innerHeight
+  let hero_height_proportion = 1;
+
+  window.onload = () => {
+    const heroSection = document.getElementById("hero-section");
+    if (heroSection) {
+      hero_height_proportion =
+        heroSection.offsetHeight / document.body.scrollHeight;
+    }
+  };
+
+  const screen_height = window.innerHeight;
   const { scrollYProgress } = useScroll();
-  const hero_height_proportion = document.getElementById("hero")!.offsetHeight/document.body.scrollHeight;
-  
-  const scale = useTransform(scrollYProgress, [0, hero_height_proportion], [1, 1.4]);
-  const y = useTransform(scrollYProgress, [0, hero_height_proportion], [1, screen_height/2+48]);
+
+  const scale = useTransform(
+    scrollYProgress,
+    [0, hero_height_proportion],
+    [1, 1.4]
+  );
+  const y = useTransform(
+    scrollYProgress,
+    [0, hero_height_proportion],
+    [1, screen_height / 2 + 48]
+  );
   return (
-    <div id="hero" className="relative flex-col flex min-h-screen bg-white  md:bg-cover bg-contain bg-no-repeat bg-hero-pattern bg-center z-10">
+    <div
+      id="hero"
+      className="relative flex-col flex min-h-screen bg-white  md:bg-cover bg-contain bg-no-repeat bg-hero-pattern bg-center z-10"
+    >
       <div className="flex justify-center items-center min-h-screen">
         <motion.h1
-          style={{ scale: scale, y: y }} 
+          style={{ scale: scale, y: y }}
           className="font-serif font-bold text-black text-5xl sm:text-8xl z-10"
         >
           Mor Asraf
