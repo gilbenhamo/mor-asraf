c7f4bb3258a6d3532ca948da59e608e19d187df7
gilbenhamo <gilbh859@gmail.com>
2024-05-16 21:22:04 +0300

    hd5

--- frontend/src/pages/HealthDeclaration.tsx
+++ frontend/src/pages/HealthDeclaration.tsx
@@ -103,132 +103,150 @@ const HealthDeclaration = () => {
   };
 
   return (
-    <div className="bg-white h-full mt-10" >
-      <SectionWrapper idName="booking-israel">
-        <SectionHead headText={`Health Declaration`} />
-        <div className=" xl:flex-row flex-col flex items-center justify-center gap-10 p-2 overflow-hidden" >
-          <div className="flex-[0.75] bg-white flex flex-col justify-center items-center">
-            <form
-              name="health-booking-form"
-              method="POST"
-              onSubmit={handleSubmit}
-              className="sm:mt-12 flex flex-col justify-center items-center gap-8 xl:w-10/12 "
-            >
-              <TwoInputsGridContainer>
-                <FormInputElement
-                  id="health-form-name"
-                  name={"name"}
-                  type={"text"}
-                  placeholder="Name"
-                  onChange={handleChange}
-                />
-                <FormInputElement
-                  id="health-form-phone"
-                  name={"phone"}
-                  type={"tel"}
-                  placeholder="Phone"
-                  onChange={handleChange}
-                />
-              </TwoInputsGridContainer>
-
-              <TwoInputsGridContainer>
-                <FormInputElement
-                  id="health-form-email"
-                  name={"email"}
-                  type={"email"}
-                  placeholder="Email"
-                  onChange={handleChange}
-                />
-                <FormInputElement
-                  id="health-form-country"
-                  name={"address"}
-                  type={"text"}
-                  placeholder={"Address"}
-                  onChange={handleChange}
-                />
-              </TwoInputsGridContainer>
-
-              <TwoInputsGridContainer>
-                <FormInputElement
-                  id="health-form-id"
-                  name={"id"}
-                  type={"text"}
-                  placeholder="ID Number"
-                  onChange={handleChange}
-                />
-
-                <div className="mt-1 flex justify-start">
-                  <DateInput
-                    id={"health-form-date1"}
-                    name={"date1"}
-                    label="Date of birth"
-                    onChange={handleDateChange}
-                    allowPast
-                    allowYearDropDown
-                  />
-                </div>
-              </TwoInputsGridContainer>
-
-              <FormTextArea
-                id="health-form-disease"
-                name={"disease"}
-                rows={1}
-                placeholder={"Do you have any disease or sensitivity?"}
-                onChange={handleChange}
-              />
-
-              <FormTextArea
-                name={"medication"}
-                rows={1}
-                placeholder={"Do you take medication regularly?"}
-                id={"health-form-anything-else"}
-                onChange={handleChange}
-              />
-
-              {/*---------Check Box Elements----------*/}
-              <div className="flex flex-col w-full gap-3 ">
-                {healthDeclarationBoxs.map((elem, index) => (
-                  <CheckBox
-                    key={index}
-                    label={elem.text}
-                    id={"health_checkbox_" + elem.id}
-                    name={"health_checkbox_" + elem.id}
+    <div className="bg-white">
+      <motion.div
+        id="booking-israel"
+        className="bg-white "
+        initial={{
+          y: 0,
+          opacity: 0,
+        }}
+        animate={{
+          y: 50,
+          opacity: 1,
+          transition: {
+            type: "spring",
+            duration: 1.25,
+            delay: 0.2,
+          },
+        }}
+      >
+        <SectionWrapper idName="booking-israel">
+          <SectionHead headText={`Health Declaration`} />
+          <div className=" xl:flex-row flex-col flex items-center justify-center gap-10 p-2 overflow-hidden">
+            <div className="flex-[0.75] bg-white flex flex-col justify-center items-center">
+              <form
+                name="health-booking-form"
+                method="POST"
+                onSubmit={handleSubmit}
+                className="sm:mt-12 flex flex-col justify-center items-center gap-8 xl:w-10/12 "
+              >
+                <TwoInputsGridContainer>
+                  <FormInputElement
+                    id="health-form-name"
+                    name={"name"}
+                    type={"text"}
+                    placeholder="Name"
                     onChange={handleChange}
                   />
-                ))}
-                <div className="flex flex-col items-center">
-                  <div className="flex flex-row justify-between w-[20rem] items-center">
-                    <div className="text-black_m">Your signature:</div>
-                    <div
-                      className="border-2 pr-2 pl-2 mt-2 mb-1 text-black_m cursor-pointer"
-                      onClick={clearSignature}
-                    >
-                      X
-                    </div>
-                  </div>
-                  <div className="border-2 w-[20rem] h-32">
-                    <SignatureCanvas
-                      ref={signatureRef}
-                      penColor="black"
-                      canvasProps={{ height: 120, width: 320 }}
+                  <FormInputElement
+                    id="health-form-phone"
+                    name={"phone"}
+                    type={"tel"}
+                    placeholder="Phone"
+                    onChange={handleChange}
+                  />
+                </TwoInputsGridContainer>
+
+                <TwoInputsGridContainer>
+                  <FormInputElement
+                    id="health-form-email"
+                    name={"email"}
+                    type={"email"}
+                    placeholder="Email"
+                    onChange={handleChange}
+                  />
+                  <FormInputElement
+                    id="health-form-country"
+                    name={"address"}
+                    type={"text"}
+                    placeholder={"Address"}
+                    onChange={handleChange}
+                  />
+                </TwoInputsGridContainer>
+
+                <TwoInputsGridContainer>
+                  <FormInputElement
+                    id="health-form-id"
+                    name={"id"}
+                    type={"text"}
+                    placeholder="ID Number"
+                    onChange={handleChange}
+                  />
+
+                  <div className="mt-1 flex justify-start">
+                    <DateInput
+                      id={"health-form-date1"}
+                      name={"date1"}
+                      label="Date of birth"
+                      onChange={handleDateChange}
+                      allowPast
+                      allowYearDropDown
                     />
                   </div>
-                </div>
-              </div>
+                </TwoInputsGridContainer>
 
-              <motion.button
-                whileTap={{ scale: 0.9 }}
-                whileHover={{ scale: 1.1 }}
-                type="submit"
-                className="bg-gray_m hover:bg-black_m py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-2xl"
-              >
-                {"Send"}
-                {/* {loading ? "Sending..." : "Send"} */}
-              </motion.button>
-            </form>
+                <FormTextArea
+                  id="health-form-disease"
+                  name={"disease"}
+                  rows={1}
+                  placeholder={"Do you have any disease or sensitivity?"}
+                  onChange={handleChange}
+                />
+
+                <FormTextArea
+                  name={"medication"}
+                  rows={1}
+                  placeholder={"Do you take medication regularly?"}
+                  id={"health-form-anything-else"}
+                  onChange={handleChange}
+                />
+
+                {/*---------Check Box Elements----------*/}
+                <div className="flex flex-col w-full gap-3 ">
+                  {healthDeclarationBoxs.map((elem, index) => (
+                    <CheckBox
+                      key={index}
+                      label={elem.text}
+                      id={"health_checkbox_" + elem.id}
+                      name={"health_checkbox_" + elem.id}
+                      onChange={handleChange}
+                    />
+                  ))}
+                  <div className="flex flex-col items-center">
+                    <div className="flex flex-row justify-between w-[20rem] items-center">
+                      <div className="text-black_m">Your signature:</div>
+                      <div
+                        className="border-2 pr-2 pl-2 mt-2 mb-1 text-black_m cursor-pointer"
+                        onClick={clearSignature}
+                      >
+                        X
+                      </div>
+                    </div>
+                    <div className="border-2 w-[20rem] h-32">
+                      <SignatureCanvas
+                        ref={signatureRef}
+                        penColor="black"
+                        canvasProps={{ height: 120, width: 320 }}
+                      />
+                    </div>
+                  </div>
+                </div>
+
+                <motion.button
+                  whileTap={{ scale: 0.9 }}
+                  whileHover={{ scale: 1.1 }}
+                  type="submit"
+                  className="bg-gray_m hover:bg-black_m py-3 px-8 outline-none w-fit text-white font-bold shadow-md shadow-primary rounded-2xl"
+                >
+                  {"Send"}
+                  {/* {loading ? "Sending..." : "Send"} */}
+                </motion.button>
+              </form>
+            </div>
           </div>
-        </div>
-      </SectionWrapper>
+        </SectionWrapper>
+      </motion.div>
     </div>
   );
 };
