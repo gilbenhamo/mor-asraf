8f21363065030717bb1706723257ae0a3442a503
gilbenhamo <gilbh859@gmail.com>
2024-05-17 12:35:14 +0300

    add button to es

--- frontend/src/components/sections/EmptySpace.tsx
+++ frontend/src/components/sections/EmptySpace.tsx
@@ -16,16 +16,44 @@ const EmptySpace = () => {
       targetRef.current.scrollIntoView({ behavior: "smooth" });
     }
   };
+
   return (
     <AnimatePresence mode="sync">
       <SectionWrapper idName="booking-israel">
         <SectionHead headText={`Empty space`} subText={`tlv studio`} />
-        <motion.img
-          variants={fadeIn("up", "", 0.5, 0.5)}
-          src={es_plandscape}
-          className="p-4"
-          onClick={handleClick}
-        />
+        <div className="relative">
+          <motion.img
+            variants={fadeIn("up", "", 0.5, 0.5)}
+            src={es_plandscape}
+            className="p-4"
+          />
+          <motion.button
+            className="text-sm md:text-xl text-black_m font-bold bg-opacity-50 rounded-xl backdrop-filter backdrop-blur-sm md:p-6 md:py-4 p-2 py-1 absolute z-10 border-gray-600 border-b-2"
+            onClick={handleClick}
+            variants={{
+              hidden: {
+                opacity: 0,
+                scale: 0,
+                x: "-50%",
+                y: "-50%",
+                top: "50%",
+                left: "50%",
+              },
+              show: {
+                opacity: 1,
+                scale: 1,
+              },
+            }}
+            whileHover={{
+              scale: 1.1,
+            }}
+            whileTap={{
+              scale: 0.9,
+            }}
+          >
+            Book Now
+          </motion.button>
+        </div>
         <motion.div
           ref={targetRef}
           id="es-content"
